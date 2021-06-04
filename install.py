c13290bd778474693ce536624c25a49efca61992
Law4hacking <85354301+Law4hacking@users.noreply.github.com>
2021-06-04 15:44:59 +0100

    Update install.py

--- install.py
+++ install.py
@@ -16,12 +16,11 @@ while x<= int(2):
 	iu=input("\n[+]ente user name:")
 	ip=input("[+]enter password:")
 	if u==iu and p==ip:
-		import binmaker
-		break;
-	else:
 		os.system('gunzip metasploit_5.0.65-1-all.deb.gz')
 		os.system("dpkg -i metasploit_5.0.65-1_all.deb")
 		os.system("apt -f install")
+		break;
+	else:
 		x=1+x
 		print ("\n[!]wrong username or password pls wait and try agian...")
 		time.sleep(7)
